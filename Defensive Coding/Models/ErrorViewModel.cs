4aa9d8ebfdb9f6f3f04851d4b8b97a9f423dc7b7
Eric J Fisher <eric-fisher@pluralsight.com>
2019-02-04 13:24:23 -0500

    Add project files.

---
+++ Defensive Coding/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Defensive_Coding.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
